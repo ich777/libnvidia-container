cfe7def6a2f7db2655a5cb7ede2a9165fdad7383
Christoph <christophhummer@gmail.com>
2022-06-22 08:51:38 +0200

    Update compile.sh

--- source/compile.sh
+++ source/compile.sh
@@ -7,14 +7,14 @@ git checkout v$LAT_V
 # Patch nvc_ldcache.c and compile libnvidia-container
 sed -i '/if (syscall(SYS_pivot_root, ".", ".") < 0)/,+1 d' ${DATA_DIR}/libnvidia-container/src/nvc_ldcache.c
 sed -i '/if (umount2(".", MNT_DETACH) < 0)/,+1 d' ${DATA_DIR}/libnvidia-container/src/nvc_ldcache.c
-make 2>/dev/null
+make GO111MODULE=auto 2>/dev/null
 mv ${DATA_DIR}/libnvidia-container/deps/src/elftoolchain-0.7.1/libelf/'name libelf.so.1' ${DATA_DIR}/libnvidia-container/deps/src/elftoolchain-0.7.1/libelf/libelf.so.1
-if DESTDIR=${DATA_DIR}/libnvidia-container-$LAT_V make install prefix=/usr ; then
+if DESTDIR=${DATA_DIR}/libnvidia-container-$LAT_V make install prefix=/usr GO111MODULE=auto ; then
 	:
 else
 	echo "---Can't compile libnvidia-container---"
 	rm -R ${DATA_DIR}/libnvidia-container
-	exit 1
+	exit 0
 fi
 
 # Create daemon.json
